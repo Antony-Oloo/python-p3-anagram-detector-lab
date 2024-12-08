82dfbc9ccdcc2964f92bb0c9778a90958e0c07cd
antony <antoloo254@gmail.com>
2024-12-08 15:28:48 +0300

    All tests passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        matches = []
+        for word in possible_anagrams:
+            if sorted(word) == sorted_word:
+                matches.append(word)
+        
+        return matches
